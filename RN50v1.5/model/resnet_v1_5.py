73bd624830b0f2c36036ee9cc7cd9c2bea4262e1
Ben Snyder <jbsnyder@amazon.com>
2019-07-26 13:16:23 -0700

    switched loss to logit

--- RN50v1.5/model/resnet_v1_5.py
+++ RN50v1.5/model/resnet_v1_5.py
@@ -209,7 +209,8 @@ class ResnetModel(object):
                     'accuracy_top5': acc_top5
                 }
 
-                cross_entropy = tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels)
+                #cross_entropy = tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels)
+                cross_entropy = tf.losses.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
 
                 assert (cross_entropy.dtype == tf.float32)
                 tf.identity(cross_entropy, name='cross_entropy_loss_ref')
