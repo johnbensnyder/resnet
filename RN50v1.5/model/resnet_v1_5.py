952bbf9f6a43614998af511bcafca1f881dfe87a
Ben Snyder <jbsnyder@amazon.com>
2019-07-29 16:16:10 -0700

    adjust learning rate

--- RN50v1.5/model/resnet_v1_5.py
+++ RN50v1.5/model/resnet_v1_5.py
@@ -262,8 +262,8 @@ class ResnetModel(object):
                     tf.summary.scalar('learning_rate', learning_rate)
 
                     # optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=params["momentum"])
-                    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-                    optimizer = LarcOptimizer(optimizer, learning_rate=learning_rate)
+                    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate*10)
+                    optimizer = LarcOptimizer(optimizer, learning_rate=learning_rate*10)
                     optimizer = MixedPrecisionOptimizer(optimizer)
 
                     if params["apply_loss_scaling"]:
