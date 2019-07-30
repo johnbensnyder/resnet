b36828d579dc9d54d62da043ceb31fbea61f7de5
Ben Snyder <jbsnyder@amazon.com>
2019-07-30 11:35:01 -0700

    removed keras batch norm

--- RN50v1.5/model/layers/normalization.py
+++ RN50v1.5/model/layers/normalization.py
@@ -65,7 +65,7 @@ def batch_norm(
 
         inputs = tf.reshape(inputs, new_shape)
 
-    '''net = tf.contrib.layers.batch_norm(
+    net = tf.contrib.layers.batch_norm(
         inputs,
         decay=decay,
         scale=scale,
@@ -76,12 +76,13 @@ def batch_norm(
         data_format=data_format,
         center=center,
         param_initializers=param_initializers
-    )'''
-    net = tf.keras.layers.BatchNormalization(momentum=decay,
+    )
+    # this is going to require a more extensive rewrite
+    '''net = tf.keras.layers.BatchNormalization(momentum=decay,
                                              epsilon=epsilon,
                                              scale=scale,
                                              axis=1 if data_format=='NCHW' else -1,
-                                             center=center)(inputs)
+                                             center=center)(inputs)'''
 
     if input_rank == 2:
         net = tf.reshape(net, [-1, input_channels])
